a5374c39254c4cc49025711126e857ce8c0c8db5
kodexArg <gcavedal@gmail.com>
2025-06-04 13:17:14 -0300

    checkpoint

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 set -e
 
-# Install uv if not present
-pip3 install uv
-
-# Debug output for SECRET_KEY
-echo "DEBUG: SECRET_KEY is set to: ${SECRET_KEY}"
-
 # Run Django migrations
 .venv/bin/python manage.py makemigrations
 .venv/bin/python manage.py migrate
